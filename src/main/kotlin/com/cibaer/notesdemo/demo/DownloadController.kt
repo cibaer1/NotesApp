237e755ea9cca57a084dbf2dec00f7ce2e75e465
cibaer1 <ivoleonidalbert@gmail.com>
2026-03-24 15:54:13 +0100

    added download controller doesnt have functionallity yet

---
+++ src/main/kotlin/com/cibaer/notesdemo/demo/DownloadController.kt
@@ -0,0 +1,17 @@
+package com.cibaer.notesdemo.demo
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+/*
+@RestController
+class DownloadController {
+    @GetMapping("/download/{id}")
+    public ResponseEntity<ByteArray> download(@PathVariable id: String) {
+
+        return ResponseEntity<>(data, headers, HttpStatus.OK)
+    }
+
+}
+*/
